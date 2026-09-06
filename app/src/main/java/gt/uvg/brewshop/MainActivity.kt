5c7c3d79de5c4dbcc8fe7aee8e142061826f49c0
Jose_Soto <jdvsoto2006@gmail.com>
2026-09-06 09:09:54 -0600

    Estructura inicial del proyecto

---
+++ app/src/main/java/gt/uvg/brewshop/MainActivity.kt
@@ -0,0 +1,47 @@
+package gt.uvg.brewshop
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import gt.uvg.brewshop.ui.theme.BrewShopTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BrewShopTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun GreetingPreview() {
+    BrewShopTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/gt/uvg/brewshop/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package gt.uvg.brewshop.ui.theme
+
+import androidx.compose.material3.Typography
+
+val Typography = Typography()
